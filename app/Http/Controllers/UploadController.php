47c8bec180b241c05ac014736365a62ce919da89
Gaganpreet Singh <gaganpreetsingh.offsureit@gmail.com>
2023-11-24 17:21:19 +0530

    Changes

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -10,15 +10,17 @@ use Illuminate\Support\Str;
 
 class UploadController extends Controller
 {
-    public function index(Request $request) : JsonResponse
+    public function index(Request $request): JsonResponse
     {
         $request->validate([
             'file' => 'required|mimes:jpeg,png,pdf|max:5120', // Adjust the allowed file types and size as needed
         ]);
         $file = $request->file('file');
-        $filename = Str::orderedUuid().".".$file->getClientOriginalExtension();
+        $filename = Str::orderedUuid() . "." . $file->getClientOriginalExtension();
         $file->move(public_path('uploads'), $filename);
-        return ResponseHelper::success(_("message.file-upload-success"), URL("uploads/".$filename));
-
+        if (file_exists(public_path('uploads/' . $filename)))
+            return ResponseHelper::success(_("message.file-upload-success"), URL("uploads/" . $filename));
+        else
+            return ResponseHelper::error(_("message.file-upload-error"), null, 500);
     }
 }
